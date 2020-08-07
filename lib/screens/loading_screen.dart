4794b0e9f287dcd7c0ea651e85508e0f2a361e3b
Hrishikesh <pawarhrishi21@gmail.com>
2020-08-08 01:45:19 +0530

    Updated the API requests feature with the http package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,13 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const String api_key = "1500bc84dc1669b240c5daf14729910a";
+String longitude, latitude;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,24 +18,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location locationObject = Location();
     await locationObject.getCurrentLocation();
-    print(locationObject.latitude);
-    print(locationObject.longitude);
+    latitude = locationObject.latitude.toString();
+    longitude = locationObject.longitude.toString();
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$api_key';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var jsonDecodedData = networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
+        child: SpinKitWave(
+          color: Colors.white,
+          size: 50.0,
         ),
       ),
     );
