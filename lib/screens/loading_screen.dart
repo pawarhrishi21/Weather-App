2763b8224233089d84349e75dc267d0430638fed
Hrishikesh <pawarhrishi21@gmail.com>
2020-08-07 18:40:48 +0530

    Added the location access feature using the geolocator package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +8,24 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location locationObject = Location();
+    await locationObject.getCurrentLocation();
+    print(locationObject.latitude);
+    print(locationObject.longitude);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
